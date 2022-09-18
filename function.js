f2875a1476cc32306799949bcbe6c1e23af8fb98
GiriAdamDiwa <giridiwa08@gmail.com>
2022-09-18 22:00:54 +0700

    Frist Push

---
+++ function.js
@@ -0,0 +1,41 @@
+// var angka = 25;
+// var pos1,pos2,pos3;
+// var ans1,ans2,ans3;
+// a = 3
+// b = 4
+// c = 5
+// function triple(a,b,c){
+//     for (var n=2; n < angka; n++){
+//         a= 2 * n + 1
+//         b= 2 * n  * (n + 1);
+//         b= 2 * n  * (n + 1) + 1;
+
+//         if ( a < b < c){
+//             if ( a^2 + b^2 == c^2){
+//                 pos1 = a;
+//                 pos2 = b;
+//                 pos3 = c;
+//             }
+//             if(a + b + c == 25){
+//                 ans1 = a;
+//                 ans2 = b;
+//                 ans3 = c;
+//             }
+//         }
+//     }
+// }
+// console.log(ans1 + " " + ans2 + " " + ans3);
+function triple(a,b,c){
+    var count = 5;
+    if (triple = 25){
+        math.pow(a^2 + b^2 == c^2)
+        return true;
+    }else if(triple = 100){
+        math.pow(a^2 + b^2 == c^2)
+        return false;
+    }else{
+        return false;
+    }
+
+}
+console.log(triple(3,4,5)); // true
